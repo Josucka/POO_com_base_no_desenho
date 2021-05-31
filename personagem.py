23bd5dc5ae363d18d8eb8245ed2dd5c7c0e38cf8
Josué Barros <83733874+Josucka@users.noreply.github.com>
2021-05-30 21:32:09 -0300

    Young_Titans
    Taking as a basis the children's drawing young titans elaborated a basic code of POO in a little game of fiction oriented

---
+++ personagem.py
@@ -0,0 +1,10 @@
+class Personagem:
+  def __init__(self, robin, ravena, estelar, mutano, cyborg, todos):
+    self.robin = robin
+    self.ravena = ravena
+    self.estelar = estelar
+    self.mutano = mutano
+    self.cyborg = cyborg
+    self.todos = todos
+
+  
---
+++ personagem1.py
@@ -0,0 +1,8 @@
+class Personagem1:
+  def __init__(self, fantasminha_ravena, fantasminha_estelar, fantasminha_mutano, fantasminha_cyborg, todos_fantasminha):
+    self.ravena = fantasminha_ravena
+    self.estelar = fantasminha_estelar
+    self.mutano = fantasminha_mutano
+    self.cyborg = fantasminha_cyborg
+    self.todos = todos_fantasminha
+    
